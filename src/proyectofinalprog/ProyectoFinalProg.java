ce04780f0550a62a15af84db9bd867711ef8bb61
Aaron <Aaron@DESKTOP-BCLEH27>
2016-06-01 11:05:43 +0200

    commit inicial

---
+++ src/proyectofinalprog/ProyectoFinalProg.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package proyectofinalprog;
+
+/**
+ *
+ * @author Aaron
+ */
+public class ProyectoFinalProg {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Bienvenidos obxB = new Bienvenidos();
+        obxB.setVisible(true);
+       
+        
+       
+    }
+    
+}
